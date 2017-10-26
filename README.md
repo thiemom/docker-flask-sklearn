e1add933a073bcace9b406e7a82a15923251899a
Thiemo Meeuwissen <thiemom@hotmail.com>
2017-10-26 10:55:06 +0200

    updated to multi varriate regression

--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 # docker-flask-sklearn
 Basic example of a python flask api for machine learning with Docker, Flask, sklearn and Bokeh for visualization
 
-## requirements  
+## Requirements  
 Docker
 
-## How to run on Docker
+## Docker commands
 docker build . -t [name]
 interactive: 	docker run -p 3000:5000 -it [name]
-detached: 		docker run -p -d 3000:5000 [name]
+detached: 		docker run -d -p 3000:5000 [name]
+stop detached:	docker stop [CONTAINER ID]
 
 ## API examples
 localhost:3000/
 localhost:3000/chart
-localhost:3000/predict?x=0.1
+localhost:3000/data
+localhost:3000/predict?AGE=0.1
+localhost:3000/predict?AGE=0.1&RM=6
--- app/app.py
+++ app/app.py
@@ -1,27 +1,32 @@
 
 from flask import Flask, request, render_template, jsonify
+
 import numpy as np
+
 from sklearn import linear_model
 from sklearn.externals import joblib
 from sklearn import datasets
+
 import bokeh
 import bokeh.plotting as plt
 from bokeh.embed import components
 
 # toy data
-diabetes = datasets.load_diabetes()
+data = datasets.load_boston()
+data_id = 'Boston Housing Dataset'
+target_name = 'Price'
 
 # select one feature
-feature_index = 2
-feature_name = diabetes.feature_names[feature_index]
-X = diabetes.data[:, np.newaxis, feature_index]
-y = diabetes.target
+feature_index = 0
+feature_name = data.feature_names[feature_index]
+X = data.data
+y = data.target
 
 # fit model
 model = linear_model.LinearRegression()
 model.fit(X, y)
 
-# save
+# save model
 model_filename = 'model.pkl'
 joblib.dump(model, model_filename) 
 
@@ -29,18 +34,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "Hello"
+	return "Hello, from a Machine Learning Web Tool"
 
 @app.route('/predict', methods=['GET'])
 def predict():
-	x = float(request.args.get('x'))
 	loaded_model = joblib.load(model_filename)
-	prediction = loaded_model.predict(x)
-	return str(np.asscalar(prediction))
+	Xp = np.empty((1, X.shape[1]))
+	for i, feat in enumerate(data.feature_names):
+		Xp[0, i] = request.args.get(feat, default=X.mean(axis=0)[i], type=np.float)
+	yp = loaded_model.predict(Xp)
+	return jsonify(
+		data=dict(zip(data.feature_names, Xp.T.tolist())), 
+		prediction={target_name: yp.tolist()})
 
 @app.route('/data')
 def show_data():
-	return jsonify(X=X.tolist(), y=y.tolist())
+	return jsonify(
+		data=dict(zip(data.feature_names, X.tolist())), 
+		prediction={target_name: y.tolist()})
 
 @app.route('/chart')
 def chart():
@@ -48,9 +59,9 @@ def chart():
 
 	plot = scatter(X, y, 
 		model=loaded_model,
-		title='Diabetes Dataset', 
+		title=data_id, 
 		xlabel=feature_name, 
-		ylabel='Disease progression after 1 year')
+		ylabel=target_name)
 
 	script, div = components(plot)
 	head = '''
@@ -60,7 +71,7 @@ def chart():
 
 	return render_template('chart.html', 
 		page_title='Basic Machine Learning',
-		chart_title='Linear Regression Model',
+		chart_title=str(loaded_model).split('(')[0],
 		chart_head=head,
 		chart_div=div, 
 		chart_script=script)	
@@ -70,17 +81,21 @@ def scatter(X, y, model=None, title=None, xlabel=None, ylabel=None):
 	p = plt.figure(title=title, 
 			x_axis_label=xlabel, 
 			y_axis_label=ylabel)
-	p.circle(X.ravel(), y,
+	p.circle(X[:,feature_index], y,
 		fill_color='blue', 
 		fill_alpha=0.8,
 		size=8)
 	if not model is None:
-		Xp = np.linspace(X.min(), X.max(), 100).reshape(-1,1)
+		N = 100
+		Xp = np.empty((N, X.shape[1]))
+		Xp[:,:] = X.mean(axis=0)
+		Xp[:,feature_index] = np.linspace(X[:,feature_index].min(), 
+			X[:,feature_index].max(), N)
 		yp = model.predict(Xp)
-		p.line(Xp.ravel(), yp, 
+		p.line(Xp[:,feature_index], yp, 
 			line_color='red', 
 			line_width=2)
 	return p
 
 if __name__ == '__main__':
-	app.run(port=5000, host='0.0.0.0', debug=True)        
+	app.run(port=5000, host='0.0.0.0', debug=False)        
